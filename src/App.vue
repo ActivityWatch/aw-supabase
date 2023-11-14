fc996c730eb5fcc0c70348cf35f66debf7a76fca
Erik Bjäreholt <erik@bjareho.lt>
2023-11-14 09:40:17 +0100

    feat: added menubar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import Menubar from "./components/Menubar.vue";
 </script>
 
 <template>
   <div class="container mx-auto">
     <header class="text-center p-4">
-      <h1 class="text-2xl font-bold">Subscription Service</h1>
+      <h1 class="text-2xl font-bold">ActivityWatch Portal</h1>
+      <Menubar />
     </header>
     <main>
       <RouterView />
---
+++ src/components/Menubar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav class="flex justify-center space-x-4 mt-4">
+    <div class="rounded border">
+      <RouterLink to="/">Home</RouterLink>
+    </div>
+    <div class="rounded border">
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+    <div class="rounded border">
+      <RouterLink to="/settings">Settings</RouterLink>
+    </div>
+  </nav>
+</template>
