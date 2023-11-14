8ee86f39d3637fddb8c5c6c8bf6efd16da5b1305
Erik Bjäreholt <erik@bjareho.lt>
2023-11-14 11:24:30 +0100

    style: applied prettier

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-import SignIn from "../components/SignIn.vue";
-import SignUp from "../components/SignUp.vue";
+import TheWelcome from '../components/TheWelcome.vue'
+import SignIn from '../components/SignIn.vue'
+import SignUp from '../components/SignUp.vue'
 </script>
 
 <template>
