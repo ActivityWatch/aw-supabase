7df9d7c59718150d687d7e2dc8d0196a61b26a4f
Erik Bjäreholt <erik@bjareho.lt>
2023-11-14 10:19:08 +0100

    build: setup tailwind

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -4,13 +4,13 @@ import { RouterLink } from "vue-router";
 
 <template>
   <nav class="flex justify-center space-x-4 mt-4">
-    <div class="rounded border">
+    <div class="rounded border px-2 py-1">
       <RouterLink to="/">Home</RouterLink>
     </div>
-    <div class="rounded border">
+    <div class="rounded border px-2 py-1">
       <RouterLink to="/about">About</RouterLink>
     </div>
-    <div class="rounded border">
+    <div class="rounded border px-2 py-1">
       <RouterLink to="/settings">Settings</RouterLink>
     </div>
   </nav>
