fedb976178fc967f10fb808cfd391193f8c3c9a1
Erik Bjäreholt <erik@bjareho.lt>
2023-11-14 09:30:19 +0100

    chore: added init files from `npm init vue@latest`

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <header class="text-center p-4">
+      <h1 class="text-2xl font-bold">Subscription Service</h1>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
