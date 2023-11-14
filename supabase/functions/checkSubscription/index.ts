10d50aa18fc28120122aca8dc5c70b7d4572ced2
Erik Bjäreholt <erik@bjareho.lt>
2023-11-14 09:59:31 +0100

    style: applied prettier

--- supabase/functions/checkSubscription/index.ts
+++ supabase/functions/checkSubscription/index.ts
@@ -1,6 +1,6 @@
 const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-const stripe = require('stripe')(STRIPE_SECRET_KEY);
+const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
   const userId = req.query.userId || req.body.userId;
@@ -11,9 +11,9 @@ module.exports = async (req, res) => {
 
   // Fetch subscription info from Supabase
   const { data, error } = await supabase
-    .from('user_subscriptions')
-    .select('subscription_id')
-    .eq('user_id', userId);
+    .from("user_subscriptions")
+    .select("subscription_id")
+    .eq("user_id", userId);
 
   if (error || !data || !data.length) {
     return res.status(404).send("Subscription not found");
@@ -24,9 +24,9 @@ module.exports = async (req, res) => {
   // Validate subscription with Stripe
   const subscription = await stripe.subscriptions.retrieve(subscriptionId);
 
-  if (!subscription || subscription.status !== 'active') {
+  if (!subscription || subscription.status !== "active") {
     return res.status(403).send("Subscription is not active");
   }
 
-  return res.status(200).json({ status: 'active' });
+  return res.status(200).json({ status: "active" });
 };
