d8a890f546270ebb6d9737a82f306efce7b8fc3f
Erik Bjäreholt <erik@bjareho.lt>
2023-11-14 09:48:02 +0100

    fix: fixed lints

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import Menubar from "./components/Menubar.vue";
+import { RouterView } from "vue-router";
+import MenuBar from "./components/MenuBar.vue";
 </script>
 
 <template>
   <div class="container mx-auto">
     <header class="text-center p-4">
       <h1 class="text-2xl font-bold">ActivityWatch Portal</h1>
-      <Menubar />
+      <MenuBar />
     </header>
     <main>
       <RouterView />
