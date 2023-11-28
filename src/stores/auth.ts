aca8e7b8aa311e87d1486b5980b066227e36ed1c
Erik Bjäreholt <erik@bjareho.lt>
2023-11-28 17:49:55 +0100

    fix: added auth store forgotten in last commit

---
+++ src/stores/auth.ts
@@ -0,0 +1,53 @@
+import { defineStore } from 'pinia'
+import { supabase } from '../supabase/init.ts'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null
+  }),
+  actions: {
+    async signIn(email: string, password: string) {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+      })
+      if (error) throw error
+    },
+    async signUp(email: string, password: string) {
+      const { error } = await supabase.auth.signUp({
+        email,
+        password
+      })
+      if (error) throw error
+    },
+    signOut() {
+      supabase.auth.signOut()
+      this.user = null
+    },
+    async fetchUser() {
+      const {
+        data: { session },
+        error
+      } = await supabase.auth.getSession()
+      if (error) throw error
+      console.log('session', session)
+      this.user = session?.user ?? null
+    }
+  },
+  getters: {
+    isLoggedIn() {
+      return !!this.user
+    }
+  }
+})
+
+supabase.auth.onAuthStateChange(async (event, session) => {
+  console.log('auth state change')
+  console.log('event', event)
+  console.log('session', session)
+  if (event === 'SIGNED_IN' || event === 'USER_UPDATED') {
+    useAuthStore().fetchUser()
+  } else if (event === 'SIGNED_OUT') {
+    useAuthStore().user = null
+  }
+})
