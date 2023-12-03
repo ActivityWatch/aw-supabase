f76280f15a75cd9abe16fa3ae81b933f10a11abf
Erik Bjäreholt <erik@bjareho.lt>
2023-12-03 14:05:17 +0100

    fix: fixed styling, added proper icon (from media submodule), remove unused src/index.html

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,12 @@ import MenuBar from './components/MenuBar.vue'
 </script>
 
 <template lang="pug">
-div(class="dark:bg-gray-900 dark:text-gray-100")
-  div.max-w-5xl.mx-auto.h-screen.flex.flex-col
-    header
-        MenuBar
-    main.px-4
-        RouterView
-    div.flex-grow
-    footer.text-center.p-4.text-gray-400.pt-8.text-sm
-        p Website operated by #[a.link(href="https://superuserlabs.org") Superuser Labs]
+div.max-w-5xl.mx-auto.flex.flex-col
+  header
+      MenuBar
+  main.px-4
+      RouterView
+  div.flex-grow
+  footer.text-center.p-4.text-gray-400.pt-8.text-sm
+      p Website operated by #[a.link(href="https://superuserlabs.org") Superuser Labs]
 </template>
